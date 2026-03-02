7d0437c656885618a1d4f56aea3cdc79d1be0edc
D3nilsonFrancisco <d3nilson3francisco@gmail.com>
2026-03-02 01:01:50 +0100

    teste

---
+++ exercícios/ex027/testando-menu/script.js
@@ -0,0 +1,8 @@
+ const mediaQuery = Window.matchMedia("(max-width:610px)")
+ function clickmenu(){
+        if (nav.style.display == 'block'){
+            nav.style.display = 'none'
+        } else{
+            nav.style.display = 'block'
+        }
+    }
